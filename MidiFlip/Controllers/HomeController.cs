6dbf45f8742c851880b6ab25c9226ce26118d593
DennisvHest <dennisvhest@hotmail.nl>
2017-11-25 21:37:10 +0100

    Project setup + homepage styling

--- MidiFlip/Controllers/HomeController.cs
+++ MidiFlip/Controllers/HomeController.cs
@@ -10,16 +10,9 @@ namespace MidiFlip.Controllers {
             return View();
         }
 
-        public ActionResult About() {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact() {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+        [HttpPost]
+        public ActionResult Flip() {
+            return new HttpNotFoundResult();
         }
     }
 }
