653b9059fa6e0d808ddf7bda52fd71f11b42a974
DennisvHest <dennisvhest@hotmail.nl>
2017-11-26 21:56:25 +0100

    Flipping works in backend

--- MidiFlip/Controllers/HomeController.cs
+++ MidiFlip/Controllers/HomeController.cs
@@ -1,18 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using MidiFlip.Services;
 
 namespace MidiFlip.Controllers {
     public class HomeController : Controller {
+
+        private readonly IMidiService _midiService;
+
+        public HomeController(IMidiService midiService) {
+            _midiService = midiService;
+        }
+
         public ActionResult Index() {
             return View();
         }
 
         [HttpPost]
         public ActionResult Flip() {
-            return new HttpNotFoundResult();
+            if (Request.Files.Count != 1)
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError);
+
+            if (Request.Files[0]?.ContentType != "audio/midi" && Request.Files[0]?.ContentType != "audio/mid")
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError);
+
+            Stream flippedMidi = _midiService.Flip(Request.Files[0]);
+
+            return File(flippedMidi, "audio/midi", "flipped");
         }
     }
 }
---
+++ MidiFlip/Services/MidiService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MidiSharp;
+using MidiSharp.Events;
+using MidiSharp.Events.Voice.Note;
+
+namespace MidiFlip.Services {
+    public interface IMidiService {
+        Stream Flip(HttpPostedFileBase midiFile);
+    }
+
+    public class MidiService : IMidiService {
+
+        public Stream Flip(HttpPostedFileBase midiFile) {
+            //Load midi file
+            MidiSequence midi = MidiSequence.Open(midiFile.InputStream);
+
+            foreach (MidiTrack track in midi.Tracks) {
+                //The anchor note to flip everything else around
+                IEnumerable<OnNoteVoiceMidiEvent> onNoteEvents = track.OfType<OnNoteVoiceMidiEvent>();
+                int anchorNote = onNoteEvents.First().Note;
+
+                foreach (OnNoteVoiceMidiEvent onNoteEvent in onNoteEvents) {
+                    onNoteEvent.Note = (byte) (anchorNote + anchorNote - onNoteEvent.Note);
+                }
+            }
+
+            //Create flipped MIDI-file
+            Stream flippedMidiStream = new MemoryStream();
+            midi.Save(flippedMidiStream);
+            flippedMidiStream.Position = 0;
+
+            return flippedMidiStream;
+        }
+    }
+}
