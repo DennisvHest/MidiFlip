f9aad4eb59c9191019b1b09ff5cbe49675beb1b8
Dennis van Hest <dennisvhest@hotmail.nl>
2018-02-02 19:47:42 +0100

    Category search styling

---
+++ MidiFlip/Helpers/HtmlExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using MidiFlip.Enums;
+
+namespace MidiFlip.Helpers {
+
+    public static class HtmlExtensions {
+
+        public static string GetCategoryFaClass(this HtmlHelper helper, CategoryType type) {
+            switch (type) {
+                case CategoryType.Artist:
+                    return "user";
+                case CategoryType.MovieTheme:
+                    return "film";
+                case CategoryType.NationalAnthem:
+                    return "globe";
+                case CategoryType.Seasonal:
+                    return "leaf";
+                case CategoryType.TvTheme:
+                    return "television";
+                case CategoryType.VideoGame:
+                    return "gamepad";
+                default:
+                    return "tag";
+            }
+        }
+    }
+}
